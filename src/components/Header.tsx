d8016235910b31ca924224e5ca82a947ad2cafab
krylov72 <qobject1337@gmail.com>
2024-07-14 23:18:54 +0500

    create header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+
+
+export const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://logo-base.com/logo/playstation_logo_logotype.png" alt="" />
+        </header>
+    )
+}
