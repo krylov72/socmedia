d8a8c76c95890a5adc12865739b7fe1bccec188f
krylov72 <qobject1337@gmail.com>
2024-07-14 23:19:10 +0500

    create profile component

---
+++ src/components/Profile.tsx
@@ -0,0 +1,23 @@
+
+
+export const Profile = () => {
+    return (
+      <div className='content'>Hello
+      <img src="https://siabit.ru/wp-content/uploads/2023/10/5.jpg" alt="" />
+      <div>
+        <img src="https://avatars.mds.yandex.net/i?id=43ceb99d45f9d9a4d78cd04d9329f9c18c2f687dde0f975c-4860157-images-thumbs&n=13" alt="" />
+
+      </div>
+      <div>
+        My posts
+        <div>New post</div>
+      </div>
+      <div>
+        post 1
+      </div>
+      <div>
+        post 2
+      </div>
+    </div>
+    )
+}
