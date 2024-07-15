0664c7631fd4f65b361eea0624d97d84c9ad16d5
krylov72 <qobject1337@gmail.com>
2024-07-15 19:04:41 +0500

    Add imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Header } from './components/Header';
-import { NavBar } from './components/NavBar';
-import { Profile } from './components/Profile';
+import { Header } from './components/Header/Header';
+import { NavBar } from './components/Navbar/NavBar';
+import { Profile } from './components/Profile/Profile';
 
 
 function App() {
