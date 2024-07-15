e0e4ba9d3d5f7a4c5fb46c41094a35f67c63b859
krylov72 <qobject1337@gmail.com>
2024-07-15 19:02:45 +0500

    Delete untracked components

--- src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export const Header = () => {
-    return (
-        <header className='header'>
-            <img src="https://logo-base.com/logo/playstation_logo_logotype.png" alt="" />
-        </header>
-    )
-}
