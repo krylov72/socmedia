e8f03abb027e190f565f91aa3f3599d0117214b0
krylov72 <qobject1337@gmail.com>
2024-07-17 17:58:53 +0500

    Add Hash Router

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,37 @@ import './App.css';
 import { Header } from './components/Header/Header';
 import { NavBar } from './components/Navbar/NavBar';
 import { Profile } from './components/Profile/Profile';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Messages } from './components/Dialogs/Messages';
+
+export const routes = {
+  profile: '/profile',
+  messages:'/messages',
+  news:'/news',
+  music:'/music',
+  settings:'/settings'
+}
 
 
 function App() {
   return (
+    <BrowserRouter>
     <div className='app-wrapper'>
       <Header />
       <NavBar />
-      <Profile />
+      <div className='app-wrapper-content'>
+        <Routes>
+          <Route path={routes.profile} element={<Profile/>} />
+          <Route path={routes.messages} element={<Messages/>} />
+          <Route path={routes.news} element={<Profile/>} />
+          <Route path={routes.music} element={<Profile/>} />
+          <Route path={routes.settings} element={<Profile/>} />
+        </Routes>
+        {/* <Profile /> */}
+      </div>
     </div>
+    </BrowserRouter>
+    
   );
 }
 
