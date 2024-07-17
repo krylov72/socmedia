9e653d4e6ba460c387f5b58242140cecebba08ab
krylov72 <qobject1337@gmail.com>
2024-07-17 18:00:52 +0500

    Refactoring insides

--- src/components/Profile/Posts/Post/Post.tsx
+++ src/components/Profile/Posts/Post/Post.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import s from './Post.module.css'
 
 type PropsType = {
-    message:string
-    likeCount?:number
+    message: string
+    likeCount?: number
 }
 
-export const Post = ({message,likeCount = 0}:PropsType) => {
+export const Post = ({ message, likeCount = 0 }: PropsType) => {
     return (
         <div className={s.post} >
-            <img src="https://avatars.mds.yandex.net/i?id=80f47dad1ad4ca5e92484431eea651cd-5258253-images-thumbs&n=13" alt="" />
-            <p>{message}</p>
-            <p>like: <span>{likeCount}</span> </p> 
+            <img src="https://www.pngarts.com/files/11/Avatar-Transparent-Images.png" alt="" />
+            <div className={s.postInfo}>
+                <p>{message}</p>
+                <p>like: <span>{likeCount}</span> </p>
+            </div>
         </div>
     );
 };
