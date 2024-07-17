e91766894d882938ac098072a995142b65c8c2a4
krylov72 <qobject1337@gmail.com>
2024-07-17 18:00:16 +0500

    Refactoring for Hash Router

--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -1,14 +1,36 @@
+import { NavLink } from 'react-router-dom'
+import { routes } from '../../App'
 import s from './Navbar.module.css'
 
 export const NavBar = () => {
     return (
         <nav className={s.nav}>
         <ul>
-          <li><a href="">Profile</a></li>
-          <li><a href="">Messages</a></li>
-          <li><a href="">News</a></li>
-          <li><a href="">Music</a></li>
-          <li><a href="">Settings</a></li>
+          <li>
+            <NavLink to={routes.profile} className={({isActive}) =>
+    isActive ? s.active : ""
+  }>Profile</NavLink>
+            </li>
+          <li>
+            <NavLink to={routes.messages} className={({isActive}) =>
+    isActive ? s.active : ""
+  }>Messages</NavLink>
+            </li>
+          <li>
+            <NavLink to={routes.news} className={({isActive}) =>
+    isActive ? s.active : ""
+  }>News</NavLink>
+            </li>
+          <li>
+            <NavLink to={routes.music} className={({isActive}) =>
+    isActive ? s.active : ""
+  }>Music</NavLink>
+            </li>
+          <li>
+            <NavLink to={routes.settings} className={({isActive}) =>
+    isActive ? s.active : ""
+  }>Settings</NavLink>
+            </li>
         </ul>
       </nav>
     )
